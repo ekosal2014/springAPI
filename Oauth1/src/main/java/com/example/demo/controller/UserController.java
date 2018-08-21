8f744def818cad18241ffcdaa49e88db3f7da7a6
ekosal2014 <ekosal2014@gmail.com>
2018-08-21 18:04:49 +0900

    sdfgsd

--- Oauth1/src/main/java/com/example/demo/controller/UserController.java
+++ Oauth1/src/main/java/com/example/demo/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.User;
@@ -33,8 +34,8 @@ public class UserController {
 	        return "success";
 	    }
 	    
-	    @RequestMapping(value = "/user/{name}", method = RequestMethod.GET)
-	    public User getUserById(@PathVariable(value = "name") String name){
+	    @RequestMapping(value = "/user-by-name", method = RequestMethod.GET)
+	    public User getUserById(@RequestParam(value = "name") String name){
 	        //userService.findByName(id);
 	        return userService.findByName(name);
 	    }
